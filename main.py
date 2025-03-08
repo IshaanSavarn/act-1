3cc6a12d3990d861df8039bd54e0c5630c44969a
IshaanSavarn <isgamerishaan@gmail.com>
2025-03-08 20:00:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+Name="Ishaan"
+print("My name is",Name)
+
+age=20
+print("My age is",age)
+
+grade=20
+print("My grade is",grade)
+
+is_student=True
+print("I am a student",is_student)
+
+gender="male"
+print("I am a :",gender)
+      
